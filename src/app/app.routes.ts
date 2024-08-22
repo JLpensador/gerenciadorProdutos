6d8277505b916247b9c43759188d654dd6885a96
JLpensador <joaoluizborgesmorais@gmail.com>
2024-08-21 21:02:31 -0300

    Refactor: Cria funções resolver especializadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,13 @@
-import {
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  Routes,
-} from '@angular/router';
+import { Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component';
-import { CreateComponent } from './features/create/create.component';
-import { ProductsService } from './shared/services/products.service';
-import { inject } from '@angular/core';
+import { getProducts } from './shared/resolvers/get-products.resolver';
+import { getProduct } from './shared/resolvers/get-product.resolver';
 
 export const routes: Routes = [
   {
     path: '',
     resolve: {
-      products: () => {
-        const productsService = inject(ProductsService);
-        return productsService.getAll();
-      },
+      products: getProducts,
     },
     component: ListComponent,
   },
@@ -29,10 +21,7 @@ export const routes: Routes = [
   {
     path: 'edit-product/:id',
     resolve: {
-      product: (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-        const productsService = inject(ProductsService);
-        return productsService.get(route.paramMap.get('id') as string);
-      },
+      product: getProduct,
     },
     loadComponent: () =>
       import('./features/edit/edit.component').then((m) => m.EditComponent),
