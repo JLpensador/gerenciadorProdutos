7252c81fdcfa1303d0bb4e32c35510776c513934
JLpensador <joaoluizborgesmorais@gmail.com>
2024-08-23 17:58:05 -0300

    Feat: Adiciona Links para o GitHub e para o Linkedin ao Projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { LinksComponent } from './shared/components/links/links.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, LinksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/shared/components/links/links.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-links',
+  standalone: true,
+  imports: [],
+  templateUrl: './links.component.html',
+  styleUrl: './links.component.scss'
+})
+export class LinksComponent {
+
+}
